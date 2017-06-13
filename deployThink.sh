a97c716e7b42576f82aa3925d288b0d72dd575b9
winston_hsieh <winston_hsieh@gemteks.com>
2017-06-13 11:13:05 +0800

    Comment for creating default services.

--- deployThink.sh
+++ deployThink.sh
@@ -20,9 +20,9 @@
 cf login -a https://api.ng.bluemix.net
 
 # create service
-cf create-service cloudantNoSQLDB Lite Humix-Cloudant-Service
-cf create-service conversation standard Humix-Conversation-Service
-cf create-service speech_to_text standard Humix-Speech-Service
+#cf create-service cloudantNoSQLDB Lite Humix-Cloudant-Service
+#cf create-service conversation standard Humix-Conversation-Service
+#cf create-service speech_to_text standard Humix-Speech-Service
 #cf create-service text_to_speech standard Humix-TTS-Service
 
 # deploy application
